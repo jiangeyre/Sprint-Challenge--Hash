7d01d8f940eabeed31505a118b30426875e032d9
Lexie Jiang <lexiemingjiang@gmail.com>
2020-05-08 09:32:00 -0700

    Commit 3: ex3.py complete, tests OK, a bit slow, might refactor later

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,28 @@
 def intersection(arrays):
+    # instantiate dict for key value - num and count
+    itemcount = dict()
+    # initialize array to add the intersections
+    result = []
 
-    """
-    YOUR CODE HERE
-    """
+    # iteration over the arr list
+    for i, nums in enumerate(arrays):
+        # then iterate over the list in the larger list
+        for num in nums:
+            # if item has a count already and index > 1, we increment the count
+            if itemcount.get(num) is not None and i > 0:
+                # num becomes the key and increment curr val by one
+                itemcount[num] = itemcount[num] + 1
+            # if no entry with num in dict set index to one
+            elif itemcount.get(num) is None and i == 0:
+                itemcount[num] = 1
+            else:
+                continue
+
+    for numb in itemcount:
+        # if number count == array.length()
+        if itemcount[numb] == len(arrays):
+            # append the number to the intersection list
+            result.append(numb)
 
     return result
 
